d6f028e07cf808e004cec69091757f6df507a00e
KushagraKha0 <kharek@student.unimelb.edu.au>
2024-03-29 20:40:09 +1100

    Create test_basic.py
    Created tests folder for testing

---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[0]
+sys.path.append(str(path_root))
+
+def test_assertions():
+    string = True
+    assert True, "Something is wrong with the Github Workflow - please contact kharek@student.unimelb.edu.au"
