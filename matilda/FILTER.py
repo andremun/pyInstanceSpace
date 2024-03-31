cf16f1691d23a27e1c60ca68be34156dd5826aca
junhengc <junhengc3382@gmail.com>
2024-04-01 04:44:42 +1100

    fix: proposed in Model. feat: 2 new stub

---
+++ matilda/FILTER.py
@@ -0,0 +1,21 @@
+from typing import List
+
+import numpy as np
+from numpy._typing import NDArray
+
+from matilda.model import Opts, Model
+
+"""
+By Chen
+
+    Note: NOT quite sure where this function were used
+    In the buildIS, this function's x parameter's size could be changed,
+    so I treat x type is NDArray[np.double], rather than Model.data.X
+
+"""
+
+def FILTER(x: NDArray[np.double], y: NDArray[np.double], opts: Opts.selvars) -> \
+        List[NDArray[bool], NDArray[bool], NDArray[bool]]:
+
+    # TODO: Rewrite FILTER logic in python
+    raise NotImplementedError
--- matilda/PRELIM.py
+++ matilda/PRELIM.py
@@ -3,6 +3,6 @@ from numpy.typing import NDArray
 from typing import List
 from model import Data, PrelimOut, Opts
 
-def PRELIM(X: NDArray[np.double], Y: NDArray.np.double, opts: Opts) -> List[Data, PrelimOut]:
+def PRELIM(X: NDArray[np.double], Y: NDArray.np.double,opts: Opts) -> List[Data, PrelimOut]:
     # TODO: Rewrite PRELIM logic in python
     raise NotImplementedError
