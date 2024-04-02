01161f97a3d986fc26e539e14a7a0e196e3a5604
Xi3xi <18651622081@163.com>
2024-04-02 22:47:31 +1100

    FUNC: Add stub for PYTHIA.m

---
+++ matilda/PYTHIA.py
@@ -0,0 +1,83 @@
+import numpy as np
+from numpy.typing import NDArray
+from sklearn.svm import SVC
+
+from model import Opts
+from typing import List, Optional
+
+@dataclass
+class AlgorithmSummary:
+    name: str
+    avg_perf_all_instances: Optional[float]
+    std_perf_all_instances: Optional[float]
+    probability_of_good: Optional[float]
+    avg_perf_selected_instances: Optional[float]
+    std_perf_selected_instances: Optional[float]
+    cv_model_accuracy: Optional[float]
+    cv_model_precision: Optional[float]
+    cv_model_recall: Optional[float]
+    box_constraint: Optional[float]
+    kernel_scale: Optional[float]
+
+def PYTHIA(Z: np.NDArray[np.double], Y: NDArray[np.double], Ybin: NDArray[np.double], Ybest: NDArray[np.double],
+           algolabels: List[str], opts: Opts) -> List[AlgorithmSummary]:
+    """
+    PYTHIA function for algorithm selection and performance evaluation using SVM.
+
+    :param Z: Feature matrix (instances x features).
+    :param Y: Target variable vector (not used directly in this function, but part of the interface).
+    :param Ybin: Binary matrix indicating success/failure of algorithms.
+    :param Ybest: Vector containing the best performance of each instance.
+    :param algolabels: List of algorithm labels.
+    :param opts: Dictionary of options.
+
+    :return: Summary of performance for each algorithm.
+    """
+    print('  -> Initializing PYTHIA.')
+    
+    # TODO Section 1: Initialize and standardize the dataset.
+
+
+    # TODO Section 2: Configure the SVM training process. 
+    # (Including kernel function selection, library usage, hyperparameter strategy, and cost-sensitive classification.)
+
+
+    # TODO Section 3: Train SVM model for each algorithm & Evaluate performance.
+
+
+    # TODO Section 4: SVM model selection.
+    
+
+    # TODO Section 5: SVM model selection.
+
+    # TODO Section 6: Generate output
+
+    raise NotImplementedError
+
+class svmRes:
+    svm: SVC
+    Ysub: NDArray[np.double]
+    Psub: NDArray[np.double]
+    Yhat: NDArray[np.double]
+    Phat: NDArray[np.double]
+    C: float
+    g: float
+
+def fitlibsvm(Z: NDArray[np.double], Ybin: NDArray[np.double], n_folds: int, kernel: str, params: NDArray[np.double]
+              ) -> (svmRes):
+    """
+
+    Train a SVM model using the LIBSVM library.
+    """
+    raise NotImplementedError
+
+
+def fitmatsvm(Z: NDArray[np.double], Ybin: NDArray[np.double], W: NDArray[np.double], cp, k: str, params: NDArray[np.double]
+              ) -> (svmRes):
+    """
+
+    Train a SVM model using MATLAB's 'fitcsvm' function.
+
+    :param cp: Cross-validation splitting strategy from package/lib
+    """
+    raise NotImplementedError
