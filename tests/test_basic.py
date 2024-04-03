4ab5a18b0f1e2375938653fd30b8a53dc50dd338
CZLam7 <czlam100@gmail.com>
2024-04-03 18:40:26 +1100

    creating test cases for fetching data from matlab

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,36 @@
 from pathlib import Path
 import sys
-path_root = Path(__file__).parents[0]
+import subprocess
+path_root = Path(__file__).parents[0].parents[0].parents[0]
 sys.path.append(str(path_root))
 
 def test_assertions():
-    string = True
-    assert True, "Something is wrong with the Github Workflow - please contact kharek@student.unimelb.edu.au"
+    matlab_script = str(path_root) + '/InstanceSpace/example.m'
+    process = subprocess.Popen(["matlab", "-nodisplay", "-nosplash", "-nodesktop", "-r", f"run('{matlab_script}');exit;"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode == 0:
+        print("MATLAB code executed successfully.")
+        print("Output:")
+        print(stdout.decode())
+        assert True
+    else:
+        print("Error executing MATLAB code:")
+        print(stderr.decode())
+        assert False
+    # string = True
+    # print("this is as test case")
+    # assert True, "Something is wrong with the Github Workflow - please contact kharek@student.unimelb.edu.au"
+
+def import_matlab():
+    matlab_script = str(path_root) + '/InstanceSpace/example.m'
+    process = subprocess.Popen(["matlab", "-nodisplay", "-nosplash", "-nodesktop", "-r", f"run('{matlab_script}');exit;"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode == 0:
+        print("MATLAB code executed successfully.")
+        print("Output:")
+        print(stdout.decode())
+        assert True
+    else:
+        print("Error executing MATLAB code:")
+        print(stderr.decode())
+        assert False
