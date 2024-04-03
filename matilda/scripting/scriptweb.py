a40548f063881fe0f6823bf002638fef1d55ac38
junhengc <junhengc3382@gmail.com>
2024-04-04 02:51:46 +1100

    feat. scriptweb stub

---
+++ matilda/scripting/scriptweb.py
@@ -0,0 +1,8 @@
+from matilda.data.model import Model
+
+"""
+By Chen
+"""
+def scriptweb(contianer: Model, rootdir: str):
+    # TODO: Rewrite scriptweb logic in python
+    raise NotImplementedError
