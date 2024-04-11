b52a0cd23aec5fe4a535222bcb92805c96c813fd
Tao <nbyutao05@163.com>
2024-04-11 16:11:43 +1000

    Finished removing instances with too many missing values

--- matilda/build.py
+++ matilda/build.py
@@ -21,8 +21,15 @@ Example usage:
 """
 
 import sys
+import warnings
+
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
 
 from matilda.data.model import Model
+
+MAX_DUPLICATES_RATIO = 0.5  # Constant
 
 
 def build_instance_space(rootdir: str) -> Model:
@@ -39,3 +46,35 @@ def build_instance_space(rootdir: str) -> Model:
 if __name__ == "__main__":
     rootdir = sys.argv[1]
     build_instance_space(rootdir)
+
+
+def data_processing(idx: NDArray[np.bool_], model: Model) -> int:  # fix late
+    """
+    Process data for instance space analysis.
+
+    :param idx: A boolean array indicating features to be retained or removed.
+    """
+    if idx is not None:
+        # Remove instances with too many missing values
+        warnings.warn(
+            "There are features with too many missing values. They are\
+being removed to increase speed."
+        )
+        model.data.x = model.data.x.loc[:, ~idx]
+        model.data.feat_labels = [
+            label for label, keep in zip(model.data.feat_labels, ~idx) if keep
+        ]
+
+    # get the number of instances and unique instances
+    ninst = model.data.x.shape[0]
+    nuinst = len(model.data.x.drop_duplicates())
+
+    # check if there are too many repeated instances
+    if nuinst / ninst < MAX_DUPLICATES_RATIO:
+        warnings.warn(
+            "-> There are too many repeated instances. It is unlikely\
+that this run will produce good results."
+        )
+
+    # Storing the raw data for further processing, e.g., graphs
+    return 1
