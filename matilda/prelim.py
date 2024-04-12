a8e517263e591e1aac92e4164c581b4eabacb0ff
KIAND-glitch <kiand@student.unimelb.edu.au>
2024-04-12 13:46:34 +1000

    add main func to run and verify prelim works

--- matilda/prelim.py
+++ matilda/prelim.py
@@ -10,19 +10,23 @@ outlier detection and removal, and binary performance classification. These task
 guided by the options specified in the `Opts` object.
 """
 
+import csv
+from pathlib import Path
 import numpy as np
 from numpy.typing import NDArray
 from scipy import stats
 
 from matilda.data.model import Data, PrelimOut
 from matilda.data.option import Opts
-
+import json
 
 def prelim(
     x: NDArray[np.double],
     y: NDArray[np.double],
-    opts: Opts,
-) -> tuple[Data, PrelimOut]:
+    abs_perf, beta_threshold, epsilon, max_perf, bound, norm,  # noqa: ANN001
+    # opts: Opts,
+):
+# ) -> tuple[Data, PrelimOut]:
     """
     Perform preliminary processing on the input data 'x' and 'y'.
 
@@ -36,31 +40,31 @@ def prelim(
 
     y_raw = y.copy()
     nalgos = y.shape[1]
-    out = PrelimOut()
+    out = {}
 
     print("-------------------------------------------------------------------------")
     print("-> Calculating the binary measure of performance")
 
     msg = "An algorithm is good if its performance is "
-    if opts.perf.max_perf:
+    if max_perf:
         y_aux = y.copy()
         y_aux[np.isnan(y_aux)] = -np.inf
 
         y_best = np.max(y_aux, axis=1)
         p = np.argmax(y_aux, axis=1)
 
-        if opts.perf.abs_perf:
-            y_bin = y_aux >= opts.perf.epsilon
-            msg = msg + "higher than " + opts.perf.epsilon
+        if abs_perf:
+            y_bin = y_aux >= epsilon
+            msg = msg + "higher than " + epsilon
         else:
             y_best[y_best == 0] = np.finfo(float).eps
             y[y == 0] = np.finfo(float).eps
             y = 1 - y / y_best[:, np.newaxis]
-            y_bin = (1 - y_aux / y_best[:, np.newaxis]) <= opts.perf.epsilon
+            y_bin = (1 - y_aux / y_best[:, np.newaxis]) <= epsilon
             msg = (
                 msg
                 + "within "
-                + str(round(100 * opts.perf.epsilon))
+                + str(round(100 * epsilon))
                 + "% of the best."
             )
 
@@ -69,27 +73,30 @@ def prelim(
         y_aux[np.isnan(y_aux)] = np.inf
         y_best = np.min(y_aux, axis=1)
 
-        if opts.perf.abs_perf:
-            y_bin = y_aux <= opts.epsilon
-            msg = msg + "less than " + opts.perf.epsilon
+        if abs_perf:
+            y_bin = y_aux <= epsilon
+            # msg = msg + "less than " + epsilon
         else:
             y_best[y_best == 0] = np.finfo(float).eps
             y[y == 0] = np.finfo(float).eps
             y = 1 - y_best[:, np.newaxis] / y
-            y_bin = (1 - y_best[:, np.newaxis] / y_aux) <= opts.perf.epsilon
+            y_bin = (1 - y_best[:, np.newaxis] / y_aux) <= epsilon
             msg = (
                 msg
                 + "within "
-                + str(round(100 * opts.perf.epsilon))
+                + str(round(100 * epsilon))
                 + "% of the worst."
             )
 
     print(msg)
 
 
-    best_algos = np.equal(y_raw, y_best)
+    # best_algos = np.equal(y_raw, y_best)
+    best_algos = y_raw == y_best[:, np.newaxis]
     multiple_best_algos = np.sum(best_algos, axis=1) > 1
     aidx = np.arange(1, nalgos + 1)
+
+    p = np.zeros(y.shape[0], dtype=int)
 
     for i in range(y.shape[0]):
         if multiple_best_algos[i]:
@@ -103,44 +110,73 @@ def prelim(
     print("Random selection is used to break ties.")
 
     num_good_algos = np.sum(y_bin, axis=1)
-    beta = num_good_algos > (opts.perf.beta_threshold * nalgos)
+    beta = num_good_algos > (beta_threshold * nalgos)
 
-    if opts.bound:
+    if bound:
         print("-> Removing extreme outliers from the feature values.")
-        out.med_val = np.nanmedian(x, axis=0)
-        out.iq_range = np.subtract(*np.percentile(x, [75, 25], axis=0))
-        out.hi_bound = out.medval + 5 * out.iqrange
-        out.lo_bound = out.medval - 5 * out.iqrange
-        himask = x > out.hi_bound
-        lomask = x < out.lo_bound
-        x = np.where(himask | lomask, x, out.hi_bound)
-        x = np.where(lomask, x, out.lo_bound)
+        med_val = np.nanmedian(x, axis=0)
+        iq_range = np.subtract(*np.percentile(x, [75, 25], axis=0))
+        hi_bound = med_val + 5 * iq_range
+        lo_bound = med_val - 5 * iq_range
+        himask = x > hi_bound
+        lomask = x < lo_bound
+        x = np.where(himask | lomask, x, hi_bound)
+        x = np.where(lomask, x, lo_bound)
 
-    if opts.norm:
-        nfeats = x.shape[1]
-        nalgos = y.shape[1]
-        print("-> Auto-normalizing the data using Box-Cox and Z transformations.")
-        out.min_x = np.min(x, axis=0)
-        x = x - out.min_x + 1
-        out.lambda_x = np.zeros(nfeats)
-        out.mu_x = np.zeros(nfeats)
-        out.sigma_x = np.zeros(nfeats)
-        for i in range(nfeats):
-            aux = x[:, i]
-            idx = np.isnan(aux)
-            aux[~idx], out.lambda_x[i] = stats.boxcox(aux[~idx])
-            aux, out.mu_x[i], out.sigma_x[i] = stats.zscore(aux, ddof=1)
-            # ddof set to 1 as python defaults to 0; while MATLAB defaults to 1
-            x[~idx, i] = aux
+    # if norm:
+    #     nfeats = x.shape[1]
+    #     nalgos = y.shape[1]
+    #     print("-> Auto-normalizing the data using Box-Cox and Z transformations.")
+    #     min_x = np.min(x, axis=0)
+    #     x = x - min_x + 1
+    #     lambda_x = np.zeros(nfeats)
+    #     mu_x = np.zeros(nfeats)
+    #     sigma_x = np.zeros(nfeats)
+    #     for i in range(nfeats):
+    #         aux = x[:, i]
+    #         idx = np.isnan(aux)
+    #         aux[~idx], lambda_x[i] = stats.boxcox(aux[~idx])
+    #         aux, mu_x[i], sigma_x[i] = stats.zscore(aux, ddof=1)
+    #         # ddof set to 1 as python defaults to 0; while MATLAB defaults to 1
+    #         x[~idx, i] = aux
 
-        out.min_x = np.min(y)
-        y = (y - out.minY) + np.finfo(float).eps
-        out.lambda_x = np.zeros(nalgos)
-        out.mu_x = np.zeros(nalgos)
-        out.sigma_x = np.zeros(nalgos)
-        for i in range(nalgos):
-            aux = y[:, i]
-            idx = np.isnan(aux)
-            aux[~idx], out.lambda_x[i] = stats.boxcox(aux[~idx])
-            aux, out.mu_x[i], out.sigma_x[i] = stats.zscore(aux, ddof=1)
-            y[~idx, i] = aux
+    #     min_y = np.min(y)
+    #     y = (y - min_y) + np.finfo(float).eps
+    #     lambda_x = np.zeros(nalgos)
+    #     mu_x = np.zeros(nalgos)
+    #     sigma_x = np.zeros(nalgos)
+    #     for i in range(nalgos):
+    #         aux = y[:, i]
+    #         idx = np.isnan(aux)
+    #         aux[~idx], lambda_x[i] = stats.boxcox(aux[~idx])
+    #         aux, mu_x[i], sigma_x[i] = stats.zscore(aux, ddof=1)
+    #         y[~idx, i] = aux
+
+    print("p", p)
+
+def main() -> None:
+    # Create sample feature and performance data matrices
+    # read x, y from test/input/model-data-x and y
+
+    print('hello world')
+
+    x = np.genfromtxt("prelim/input/model-data-x.csv", delimiter=",")
+    y = np.genfromtxt("prelim/input/model-data-y.csv", delimiter=",")
+
+    # with open("prelim/input/options.json", "r") as f:
+    #     data = json.load(f)
+    # Create sample options
+    abs_perf = 1
+    beta_threshold = 0.5500
+    epsilon = 0.2000
+    max_perf = 0
+    bound = 1
+    norm = 1
+    # read input from json file
+
+    # Call the prelim function
+    prelim(x, y, abs_perf, beta_threshold, epsilon, max_perf, bound, norm)
+
+if __name__ == "__main__":
+    main()
+
