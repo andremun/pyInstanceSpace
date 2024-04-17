4ea0368bae001a49472b4463f350947e1b8d6475
junhengc <junhengc3382@gmail.com>
2024-04-18 00:04:55 +1000

    try again to run the test case

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,6 +3,8 @@
 import sys
 from pathlib import Path
 
+from tests.manual_selection import test_manual_selection
+
 path_root = Path(__file__).parents[0]
 sys.path.append(str(path_root))
 
@@ -15,6 +17,7 @@ def test_assertions() -> None:
         None
 
     """
+    test_manual_selection()
     #string = True
     assert True, "Something is wrong with the Github Workflow - " \
                  "please contact kharek@student.unimelb.edu.au"
