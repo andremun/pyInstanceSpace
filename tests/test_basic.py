62e3c15b0ba979e5f13082b859bcbb7bfdc90e9d
CZLam7 <czlam100@gmail.com>
2024-04-19 08:23:22 +1000

    additional details of k means

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,15 +1,6 @@
 """Test cases are here."""
 
 import sys
-<<<<<<< HEAD
-path_root = Path(__file__).parents[0].parents[0].parents[0]
-sys.path.append(str(path_root))
-
-def test_assertions():
-    string = True
-    print("this is as test case")
-    assert True, "Something is wrong with the Github Workflow - please contact kharek@student.unimelb.edu.au"
-=======
 from pathlib import Path
 
 path_root = Path(__file__).parents[0]
@@ -28,4 +19,3 @@ def test_assertions() -> None:
     assert True, "Something is wrong with the Github Workflow - " \
                  "please contact kharek@student.unimelb.edu.au"
 
->>>>>>> eec783226b664cb2cf2633de3cfc9bfe89466b50
