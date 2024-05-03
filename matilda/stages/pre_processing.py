c4f7ae1f1b0e1d349642f109a897ed1690a32e5d
junhengc <junhengc3382@gmail.com>
2024-05-03 13:23:56 +1000

    Create pre_processing.py

---
+++ matilda/stages/pre_processing.py
@@ -0,0 +1,27 @@
+
+
+class PrePro:
+    """See file docstring."""
+
+    @staticmethod
+    def run(
+        x: NDArray[np.double],
+        y: NDArray[np.double],
+        opts: Options,
+    ) -> tuple[Data, PrelimOut]:
+        """Perform preliminary processing on the input data 'x' and 'y'.
+
+        Args
+            x: The feature matrix (instances x features) to process.
+            y: The performance matrix (instances x algorithms) to
+                process.
+            opts: An object of type Options containing options for
+                processing.
+
+        Returns
+        -------
+            A tuple containing the processed data (as 'Data' object) and
+            preliminary output information (as 'PrelimOut' object).
+        """
+        # TODO: Rewrite PRELIM logic in python
+        raise NotImplementedError
