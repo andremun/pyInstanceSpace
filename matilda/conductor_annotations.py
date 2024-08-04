7815a9e1506c9d23a6b9da0672fd6f02f23f6ad4
Nathan Harvey <neathtthefirst@gmail.com>
2024-08-04 23:02:52 +1000

    start scaffolding conductor

--- matilda/conductor_annotations.py
+++ matilda/conductor_annotations.py
@@ -1,6 +1,27 @@
 """test."""
 
+from abc import ABC
+from typing import Any, TypeVar
+
+T = TypeVar("T")
+
+
+def _test_stage_decorator(base_class: type[T]) -> type[T]:
+    return base_class
+
+
+@_test_stage_decorator
 class Stage:
     """An abstract stage that can be run by the stage runner."""
 
     pass
+
+type StageInput = tuple[str, type]
+
+
+class Conductor:
+    """A generic stage manager."""
+
+    _stage_schedule: list[set[type]]
+
+    _inputs: list[StageInput]
