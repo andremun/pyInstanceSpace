b2f36a18c9d87aa5a6638acb2692b683ed70371d
ybguzel <61396020+ybguzel@users.noreply.github.com>
2024-08-28 10:17:22 +1000

    added extra test for simulated dataset
    Added a new test to check if the trace works with different trace.options.

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -3,7 +3,7 @@ from matilda.data.options import TraceOptions
 from matilda.stages.trace import Trace
 import numpy as np
 
-def test_trace():
+def test_trace_pythia():
     # Reading algo_labels from a text file with comma delimiter
     with open('test_data/trace_csvs/algolabels.txt', 'r') as f:
         algo_labels = f.read().split(',')
@@ -24,15 +24,35 @@ def test_trace():
     # Reading beta from beta.csv
     beta = np.genfromtxt('test_data/trace_csvs/beta.csv', delimiter=',', dtype=np.int8).astype(np.bool_)
 
-    # Print the initialized variables to verify
-    # print("z:", z)
-    # print("y_bin:", y_bin)
-    # print("p:", p)
-    # print("beta:", beta)
-    # print("algo_labels:", algo_labels)
+    trace_options = TraceOptions(True, 0.55)
+
+    trace = Trace(z, y_bin, p, beta, algo_labels, trace_options)
+    trace.run()
+def test_trace_simulation():
+    # Reading algo_labels from a text file with comma delimiter
+    with open('test_data/trace_csvs/algolabels.txt', 'r') as f:
+        algo_labels = f.read().split(',')
+
+
+    # Reading z from z.csv
+    z = np.genfromtxt('test_data/trace_csvs/Z.csv', delimiter=',', dtype=np.double)
+
+    # Reading y_bin from y_bin.csv
+    y_bin = np.genfromtxt('test_data/trace_csvs/yhat.csv', delimiter=',', dtype=np.int8).astype(np.bool_)
+    y_bin2 = np.genfromtxt('test_data/trace_csvs/yhat2.csv', delimiter=',', dtype=np.int8).astype(np.bool_)
+
+    # Reading p from p.csv
+    p = np.genfromtxt('test_data/trace_csvs/selection0.csv', delimiter=',', dtype=np.integer)
+    p2 = np.genfromtxt('test_data/trace_csvs/dataP.csv', delimiter=',', dtype=np.integer)
+    p = p - 1
+    p2 = p2 - 1
+    # Reading beta from beta.csv
+    beta = np.genfromtxt('test_data/trace_csvs/beta.csv', delimiter=',', dtype=np.int8).astype(np.bool_)
 
     trace_options = TraceOptions(False, 0.55)
 
     trace = Trace(z, y_bin2, p2, beta, algo_labels, trace_options)
     trace.run()
+
+
 
