823e1b3f7962c43b09a357a9bfdc931a706100ec
junhengc <junhengc3382@gmail.com>
2024-08-28 19:32:33 +1000

    reset

---
+++ matilda/scripting/script_web.py
@@ -0,0 +1,21 @@
+"""Generate eb-compatible output files.
+
+As the part of the Matilda package and is responsible for generating and
+saving web-compatible output files from a Model object.
+
+"""
+
+from matilda.data.model import Model
+
+
+def script_web(container: Model, rootdir: str) -> None:
+    """Generate and save web-compatible output files to the specified directory.
+
+    Args
+        container: The Model object containing the data to be visualized
+            and saved as web-compatible files.
+        rootdir: The root directory where the web output files will be
+            saved.
+    """
+    # TODO: Rewrite scriptweb logic in python
+    raise NotImplementedError
