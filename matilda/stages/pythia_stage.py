5cb95e3d558d5d4b30dad17ee595aca12bd8041b
KushagraKhare0 <kharek@student.unimelb.edu.au>
2024-08-30 17:20:11 +1000

    renamed to follow snake case

---
+++ matilda/stages/pythia_stage.py
@@ -0,0 +1,9 @@
+import Stage
+
+import pandas as pd
+import numpy as np
+from numpy.typing import NDArray
+
+from matilda.data.model import PythiaDataChanged, PythiaOut
+from matilda.data.options import PythiaOptions
+
