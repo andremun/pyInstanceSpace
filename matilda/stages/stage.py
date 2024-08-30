a527acc49eca8c69a6e5ffc07400bc69073cbe63
Nathan Harvey <neathtthefirst@gmail.com>
2024-08-30 17:18:30 +1000

    overwriting Stage implementation and renaming file

--- matilda/stages/stage.py
+++ matilda/stages/stage.py
@@ -1,21 +1,25 @@
-from abc import ABC
-from collections import abc
+"""Generic stage."""
+from abc import ABC, abstractmethod
+from typing import Any
+
 
 class Stage(ABC):
+    """Generic stage."""
+
     @staticmethod
-    @abc
+    @abstractmethod
     def _inputs() -> list[tuple[str, type]]:
         """Return inputs of the STAGE (run method)."""
         pass
 
     @staticmethod
-    @abc
+    @abstractmethod
     def _outputs() -> list[tuple[str, type]]:
         """Return outputs of the STAGE (run method)."""
         pass
 
     @staticmethod
-    @abc
-    def _run(*args: tuple[any]) -> tuple[any]:
+    @abstractmethod
+    def _run(*args: tuple[Any]) -> tuple[Any]:
         """Run the stage."""
         pass
