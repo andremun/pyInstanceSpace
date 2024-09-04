ee904000a77b48e02295ee38f1458445d5541858
KushagraKha <kharek@student.unimelb.edu.au>
2024-09-04 14:17:41 +1000

    Have implemented changes according to the review comments

--- tests/test_pilot_cloister.py
+++ tests/test_pilot_cloister.py
@@ -17,28 +17,31 @@ from matilda.stages.cloister import Cloister
 from matilda.stages.pilot import Pilot
 from tests.utils.option_creator import create_option
 
-script_dir = Path(__file__).parent
-default_option = create_option(cloister=CloisterOptions(p_val=0.05, c_thres=0.7))
+SCRIPT_DIR = Path(__file__).parent
+DEFAULT_OPT = create_option(cloister=CloisterOptions(p_val=0.05, c_thres=0.7))
 
 
 def test_run_analytic_pilot_cloister() -> None:
-    """Test Closister stage for analytic option from Pilot stage."""
-    fp_smapledata = script_dir / "test_data/pilot-cloister/input/test_analytic.mat"
+    """Test Closister stage for analytic option from Pilot stage.
+    
+    This test is for the analytic option from the pilot stage to determine if
+    it works for the cloister stage as well.
+    """
+    fp_smapledata = SCRIPT_DIR / "test_data/pilot-cloister/input/test_analytic.mat"
     data = loadmat(fp_smapledata)
     x_sample = data["X"]
     y_sample = data["Y"]
-    feat_labels_sample = data["featlabels"][0]
-    feat_labels_sample_str = [str(label[0]) for label in feat_labels_sample]
+    feat_labels_sample_str = [str(label[0]) for label in data["featlabels"][0]]
     opts = PilotOptions(True, 5)
     pilot = Pilot()
     _, result_pilot = pilot.run(x_sample, y_sample, feat_labels_sample_str, opts)
 
     a = result_pilot.a
 
-    cloister = Cloister(x_sample, a, default_option.cloister)
-    _, result_cloister = cloister.run(x_sample, a, default_option.cloister)
+    cloister = Cloister(x_sample, a, DEFAULT_OPT.cloister)
+    _, result_cloister = cloister.run(x_sample, a, DEFAULT_OPT.cloister)
 
-    fp_outdata = script_dir / "test_data/pilot-cloister/output/matlab_results_ana.mat"
+    fp_outdata = SCRIPT_DIR / "test_data/pilot-cloister/output/matlab_results_ana.mat"
     data_out = loadmat(fp_outdata)
 
     np.testing.assert_almost_equal(
@@ -55,8 +58,12 @@ def test_run_analytic_pilot_cloister() -> None:
 
 
 def test_run_numerical_pilot_cloister_zedge() -> None:
-    """Test Closister stage for analytic option from Pilot stage."""
-    fp_smapledata = script_dir / "test_data/pilot-cloister/input/test_numerical.mat"
+    """Test Closister stage for analytic option from Pilot stage.
+    
+    This test is for the numerical option from the pilot stage to determine if
+    it works for the cloister stage as well.
+    """
+    fp_smapledata = SCRIPT_DIR / "test_data/pilot-cloister/input/test_numerical.mat"
     data = loadmat(fp_smapledata)
     x_sample = data["X_test"]
     y_sample = data["Y_test"]
@@ -68,10 +75,10 @@ def test_run_numerical_pilot_cloister_zedge() -> None:
 
     a = result_pilot.a
 
-    cloister = Cloister(x_sample, a, default_option.cloister)
-    _, result_cloister = cloister.run(x_sample, a, default_option.cloister)
+    cloister = Cloister(x_sample, a, DEFAULT_OPT.cloister)
+    _, result_cloister = cloister.run(x_sample, a, DEFAULT_OPT.cloister)
 
-    fp_outdata = script_dir / "test_data/pilot-cloister/output/matlab_results_num.mat"
+    fp_outdata = SCRIPT_DIR / "test_data/pilot-cloister/output/matlab_results_num.mat"
     data_out = loadmat(fp_outdata)
 
     dt_out_zedge = np.sort(data_out["Zedge"][1:], axis=0)
