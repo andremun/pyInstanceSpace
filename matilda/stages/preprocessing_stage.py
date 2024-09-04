abea0ce9d410568de16a6b0a1eb2cfe90ff7f0b2
KIAND-glitch <kiand@student.unimelb.edu.au>
2024-09-04 16:59:25 +1000

    Update preprocessing_stage.py

--- matilda/stages/preprocessing_stage.py
+++ matilda/stages/preprocessing_stage.py
@@ -1,43 +1,128 @@
-"""Process the input data before running the main analysis."""
-import stage
-from pathlib import Path
 
 import numpy as np
-from sklearn.model_selection import train_test_split
+import pandas as pd
+import stage
+from numpy.typing import NDArray
 
 from matilda.data.metadata import Metadata
 from matilda.data.model import (
     Data,
-    PreprocessingDataChanged,
-    PreprocessingOut,
 )
-from matilda.data.options import InstanceSpaceOptions, PrelimOptions
-from matilda.stages.filter import Filter
+from matilda.data.options import (
+    AutoOptions,
+    BoundOptions,
+    CloisterOptions,
+    NormOptions,
+    OutputOptions,
+    PerformanceOptions,
+    PilotOptions,
+    PythiaOptions,
+    SelvarsOptions,
+    SiftedOptions,
+    TraceOptions,
+)
+
 
 class preprocessingStage(stage):
+    def __init__(self, feature_names: list[str], algorithm_names: list[str], 
+                 instance_labels: pd.Series, instance_sources: pd.Series | None,
+                 features: NDArray[np.double], algorithms: NDArray[np.double],
+                 perf: PerformanceOptions, auto: AutoOptions, bound: BoundOptions,
+                 norm: NormOptions, selvars: SelvarsOptions, sifted: SiftedOptions,
+                 pilot: PilotOptions, cloister: CloisterOptions, pythia: PythiaOptions,
+                trace: TraceOptions, outputs: OutputOptions):
+        self.feature_names = feature_names
+        self.algorithm_names = algorithm_names
+        self.instance_labels = instance_labels
+        self.instance_sources = instance_sources
+        self.features = features
+        self.algorithms = algorithms
+        self.perf = perf
+        self.auto = auto
+        self.bound = bound
+        self.norm = norm
+        self.selvars = selvars
+        self.sifted = sifted
+        self.pilot = pilot
+        self.cloister = cloister
+        self.pythia = pythia
+        self.trace = trace
+        self.outputs = outputs
 
-    def __init__(self, data: Data, options: InstanceSpaceOptions) -> None:
-        self.data = data
-        self.options = options
 
     @staticmethod
     def _inputs() -> list[tuple[str, type]]:
         return [
-            ["data", Data],
-            ["options", InstanceSpaceOptions],
+            ["feature_names", list[str]],
+            ["algorithm_names", list[str]],
+            ["instance_labels", pd.Series],
+            ["instance_sources", pd.Series | None],
+            ["features", NDArray[np.double]],
+            ["algorithms", NDArray[np.double]],
+            ["perf", PerformanceOptions],
+            ["auto", AutoOptions],
+            ["bound", BoundOptions],
+            ["norm", NormOptions],
+            ["selvars", SelvarsOptions],
+            ["sifted", SiftedOptions],
+            ["pilot", PilotOptions],
+            ["cloister", CloisterOptions],
+            ["pythia", PythiaOptions],
+            ["trace", TraceOptions],
+            ["outputs", OutputOptions],
         ]
 
     @staticmethod
     def _outputs() -> list[tuple[str, type]]:
         return [
-            ["data", PreprocessingDataChanged],
+            ["inst_labels", pd.Series],  # type: ignore[type-arg]
+            ["feat_labels", list[str]],
+            ["algo_labels", list[str]],
+            ["x", NDArray[np.double]],
+            ["y", NDArray[np.double]],
+            ["s", pd.Series | None],
         ]
 
-    def _run(self, options: InstanceSpaceOptions) -> PreprocessingOut:
+    def _run(
+        self,
+        perf: PerformanceOptions,
+        auto: AutoOptions,
+        bound: BoundOptions,
+        norm: NormOptions,
+        selvars: SelvarsOptions,
+        sifted: SiftedOptions,
+        pilot: PilotOptions,
+        cloister: CloisterOptions,
+        pythia: PythiaOptions,
+        trace: TraceOptions,
+        outputs: OutputOptions,
+    ) -> tuple[pd.Series, list[str], list[str], NDArray[np.double], NDArray[np.double],
+                pd.Series | None]:
         # All the code including the code in the buildIS should be here
         raise NotImplementedError
-    
+
     @staticmethod
-    def preprocessing(metadata: Metadata, options: InstanceSpaceOptions) -> Data:
-        # All the code for preprocessing should be here
+    def preprocessing(
+        self,
+        feature_names: list[str],
+        algorithm_names: list[str],
+        instance_labels: pd.Series,
+        instance_sources: pd.Series | None,
+        features: NDArray[np.double],
+        algorithms: NDArray[np.double],
+        perf: PerformanceOptions,
+        auto: AutoOptions,
+        bound: BoundOptions,
+        norm: NormOptions,
+        selvars: SelvarsOptions,
+        sifted: SiftedOptions,
+        pilot: PilotOptions,
+        cloister: CloisterOptions,
+        pythia: PythiaOptions,
+        trace: TraceOptions,
+        outputs: OutputOptions,
+    ) -> tuple[pd.Series, list[str], list[str], NDArray[np.double], NDArray[np.double],
+                pd.Series | None]:
+        # This has code specific to preprocessing.m
         raise NotImplementedError
+
