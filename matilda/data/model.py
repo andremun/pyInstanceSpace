perf:speed up the computation|fix:params input changed from json to opt

matilda/data/model.py
@@ -141,10 +141,10 @@ class SiftedDataChanged:
 class PilotOut:
     """Results of the Pilot process in the data analysis pipeline."""
 
-    X0: NDArray[np.double] | None  # not sure about the dimensions
-    alpha: NDArray[np.double] | None
-    eoptim: NDArray[np.double] | None
-    perf: NDArray[np.double] | None
+    X0: NDArray[np.double]  # not sure about the dimensions
+    alpha: NDArray[np.double]
+    eoptim: NDArray[np.double]
+    perf: NDArray[np.double]
     a: NDArray[np.double]
     z: NDArray[np.double]
     c: NDArray[np.double]

matilda/data/options.py
@@ -11,6 +11,8 @@ import json
 from dataclasses import dataclass, fields
 from pathlib import Path
 from typing import Any, Self, TypeVar
+import numpy as np
+from numpy.typing import NDArray
 
 import pandas as pd
 
@@ -274,8 +276,7 @@ class PythiaOptions:
     use_weights: bool
     use_grid_search: bool
 
-    # TODO: Add Params
-    # params: np.array()
+    params: NDArray[np.float]| None
     @staticmethod
     def default(
         cv_folds: int = DEFAULT_PYTHIA_CV_FOLDS,

matilda/stages/pythia.py
@@ -119,9 +119,9 @@ class Pythia:
         self.pr0sub = np.zeros((self.z.shape[0], self.nalgos), dtype=np.double)
         self.pr0hat = np.zeros((self.z.shape[0], self.nalgos), dtype=np.double)
 
-        self.preparams = self.check_precalcparams()
-        self.cp = []
-        self.svm = None
+        self.params = self.check_precalcparams(opts.params)
+        self.cp = StratifiedKFold(n_splits=opts.cv_folds, shuffle=True, random_state=0)
+        self.svm = []
         self.cvcmat = np.zeros((self.nalgos, 4), dtype=int)
         self.box_consnt = []
         self.k_scale = []
@@ -225,7 +225,7 @@ class Pythia:
                 z,
                 y_bin[:, i],
                 w[:, i],
-                skf,
+                pythia.cp,
                 opts.is_poly_krnl,
                 param_space,
                 opts.use_grid_search,
@@ -279,6 +279,8 @@ class Pythia:
             kernel=kernel,
             random_state=0,
             probability=True,
+            degree=2,
+            coef0=1
         )
         if use_grid_search:
             optimization = GridSearchCV(
@@ -336,8 +338,8 @@ class Pythia:
 
     def get_params(self, index: int) -> dict:
         """Check hyperparameters."""
-        if self.preparams is not None:
-            return self.preparams[index]
+        if self.params is not None:
+            return self.params[index]
         return self.generate_params(self.opts.use_grid_search)
 
     def record_perf(self, i: int, res: SvmRes) -> None:
@@ -424,33 +426,18 @@ class Pythia:
         self.sigma = np.std(z, ddof=1, axis=0)
         return z
 
-    def check_precalcparams(self) -> list | None:
+    def check_precalcparams(self,params) -> list | None:
         """Check pre-calculated hyper-parameters."""
-        if len(sys.argv) == IF_PARAMS_FILE:
-            try:
-                path = Path(sys.argv[1])
-                with path.open("r") as file:
-                    data = json.load(file)
-            except json.JSONDecodeError as e:
-                print(f"Error: Failed to decode JSON. {e}")
-                return None
-            if not isinstance(data, dict) or "C" not in data or "gamma" not in data:
-                print("Invalid format as parameter file.")
-                return None
-            for key in ["C", "gamma"]:
-                if not (
-                    isinstance(data[key], list)
-                    and len(data[key]) == self.nalgos
-                    and all(isinstance(i, int | float) for i in data[key])
-                ):
-                    print(f"Error: length of {key} must match to number of algorithms.")
-                    return None
-            print("-> Using pre-calculated hyper-parameters for the SVM.")
-            c_list = data.get("C", [])
-            gamma_list = data.get("gamma", [])
-            return [{"C": c, "gamma": g} for c, g in zip(c_list, gamma_list)]
-
-        return None
+        if params is None:
+            return None
+        if params.shape != (self.nalgos,2):
+            print("-> Error: Incorrect number of hyper-parameters.")
+            print("Parameters will be auto-generated.")
+            return None
+        print("-> Using pre-calculated hyper-parameters for the SVM.")
+        c_list = params[:,0]
+        gamma_list = params[:,1]
+        return [{"C": c, "gamma": g} for c, g in zip(c_list, gamma_list)]
 
     def generate_params(self, use_grid_search: bool) -> dict:
         """Generate parameters."""
@@ -572,6 +559,8 @@ if __name__ == "__main__":
         use_grid_search=True,
         use_weights=False,
         is_poly_krnl=False,
+
+        params=None
     )
 
     data_change, pythia_output = Pythia.run(