9a440df8176ac07f2c907b3d767f1da238d5170c
KushagraKha <kharek@student.unimelb.edu.au>
2024-09-18 18:58:59 +1000

    Added docstrings

--- matilda/stages/pilot.py
+++ matilda/stages/pilot.py
@@ -1,3 +1,12 @@
+"""PILOT: Obtaining a two-dimensional projection.
+
+Projecting Instances with Linearly Observable Trends (PILOT)
+is a dimensionality reduction algorithm which aims to facilitate
+the identification of relationships between instances and
+algorithms by unveiling linear trends in the data, increasing
+from one edge of the space to the opposite.
+
+"""
 from matilda.stages.stage import Stage
 
 import numpy as np
@@ -11,16 +20,42 @@ from scipy.stats import pearsonr
 from matilda.data.model import PilotDataChanged, PilotOut
 from matilda.data.options import PilotOptions
 
-class pilot(Stage):
+class pilotStage(Stage):
     def __init__(self, x: NDArray[np.double],
         y: NDArray[np.double],
         feat_labels: list[str]) -> None:
+        """Initialize the Pilot stage.
+
+        The Initialize functon is used to create a Pilot class.
+
+        Args
+        ----
+            x (NDArray[np.double]): The feature matrix (instances x features) to
+                process.
+            y (NDArray[np.double]): The data points for the selected feature
+            feat_labels (list[str]): List feature names
+
+        Returns
+        -------
+            None
+        """
         self.x = x
         self.y = y
         self.feat_labels = feat_labels
 
     @staticmethod
     def _inputs() -> list[tuple[str, type]]:
+        """Use the method for determining the inputs for pilot.
+
+        Args
+        ----
+            None
+
+        Returns
+        -------
+            list[tuple[str, type]]
+                List of inputs for the stage
+        """
         return [
             ["x", NDArray[np.double]],
             ["y", NDArray[np.double]],
@@ -29,6 +64,17 @@ class pilot(Stage):
     
     @staticmethod
     def _outputs() -> list[tuple[str, type]]:
+        """Use the method for determining the outputs for pilot.
+
+        Args
+        ----
+            None
+
+        Returns
+        -------
+            list[tuple[str, type]]
+                List of outputs for the stage
+        """
         return [
                 ["X0", NDArray[np.double] | None],  # not sure about the dimensions
                 ["alpha", NDArray[np.double] | None],
